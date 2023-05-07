7b8cdfaf03523eb38bb0d49161de1502add099f4
ashifolfi <anticream39@yahoo.com>
2023-05-06 23:03:40 -0400

    position names next to icons

--- editing.js
+++ editing.js
@@ -25,7 +25,6 @@ const createObjBtn = (objIdx, imgLoc = "") => {
 	label.innerText = nameList[objIdx]
 	
 	button.appendChild(img)
-	button.innerHTML += "<br>"
 	button.appendChild(label)
 	
 	button.addEventListener('click', () => {
